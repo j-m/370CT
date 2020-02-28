20b3aa8506c1726cc9d17771da5d3b055783dc5e
Jonathan Marsh <marshj6@coventry.ac.uk>
2020-02-28 18:40:10 +0000

    PDP-10 PDP-11: Part 1 completed, part 2 started
    Added CMake resource distribution

--- projects/concurrency/src/main.cpp
+++ projects/concurrency/src/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 
-int main(void) {
-  std::cout << "Hello concurrency" << std::endl; 
+std::vector<std::string> lines;
+
+void read() {
+  std::ifstream infile("poem.txt");
+  std::string line;
+  while (std::getline(infile, line)) {
+    lines.push_back(line);
+  }
+}
+
+void display() {
+  for (unsigned int index = 0; index < lines.size(); index++) {
+    std::cout << lines.at(index) << std::endl;
+  }
+}
+
+int main() {
+  read();
+  display();
   return 0;
 }
