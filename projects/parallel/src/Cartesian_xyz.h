b7af479673cd9bf5056e304e6ab30b225953b359
Jonathan Marsh <marshj6@coventry.ac.uk>
2020-03-02 20:45:18 +0000

    PDP-19: Started Portfolio 2 Part 4

---
+++ projects/parallel/src/Cartesian_xyz.h
@@ -0,0 +1,8 @@
+class Cartesian_xyz {
+  public:
+    int x;
+    int y;
+    int z;
+
+    Cartesian_xyz(int xCoordinate, int yCoordinate, int zCoordinate); 
+};
--- projects/parallel/src/vectorMove.cpp
+++ projects/parallel/src/vectorMove.cpp
@@ -1,15 +1,28 @@
 #include <stdio.h>
 #include <omp.h>
+#include <vector>
 
 #include "./vectorMove.h"
+#include "./Cartesian_xyz.h"
 
-#define sqr(x) ((x)*(x))
+std::vector<Cartesian_xyz*> vectors = {
+  new Cartesian_xyz(  5,  14,  10),
+  new Cartesian_xyz(  7,  -8, -14),
+  new Cartesian_xyz( -2,   9,   8),
+  new Cartesian_xyz( 15,  -6,   3),
+  new Cartesian_xyz( 12,   4,  -5),
+  new Cartesian_xyz(  4,  20,  17),
+  new Cartesian_xyz(-16,   5,  -1),
+  new Cartesian_xyz(-11,   3,  16),
+  new Cartesian_xyz(  3,  10, -19),
+  new Cartesian_xyz(-16,   7,   4)
+};
 
 void vectorMove() {
   int arr[10]= {2,3,4,5,6,7,8,9,10};
   #pragma omp parallel 
   {
     int i = omp_get_thread_num();
-    printf("%d: %d\n", i , sqr(arr[i]));
+    printf("%d: %d\n", i , arr[i]);
   }   
 }
